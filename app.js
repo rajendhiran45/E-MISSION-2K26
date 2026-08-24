afbceefb3b4154c170c9927c28e7f0caac21285d
rajendhiran45 <rajendhiranece@gmail.com>
2026-08-24 21:29:25 +0530

    Bug Fixes

--- app.js
+++ app.js
@@ -77,7 +77,6 @@ function tick() {
 }
 tick();
 setInterval(tick, 60000);
-if ("serviceWorker" in navigator) navigator.serviceWorker.register("sw.js");
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("sw.js?v=3");
 }
